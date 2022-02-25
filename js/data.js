590a717e63d767ab0d9da965ac619367fef79cc9
Toky Ratolojanahary <tokyrt.pro@gmail.com>
2022-02-25 12:56:11 +0300

    alpha version

--- js/data.js
+++ js/data.js
@@ -1,38 +1,38 @@
 //fetching skills data
 fetch("../data/skills.json")
-.then(res => {
-    return res.json();
-})
-.then(skills => {
-    var catHtml = ``;
-    skills.forEach(categorie => {
-         catHtml += `<div class="categorie">
+    .then(res => {
+        return res.json();
+    })
+    .then(skills => {
+        var catHtml = ``;
+        skills.forEach(categorie => {
+            catHtml += `<div class="categorie">
                             <h2>${categorie.categorie_name}</h2>
                             <ul class="skills">`;
-        
-                                categorie.skills.forEach(skill => {
-                                    catHtml += `<li class="skill" style="border: 1px solid ${categorie.color};">${skill}</li>`;
-                                });
 
-                catHtml += `</ul>
+            categorie.skills.forEach(skill => {
+                catHtml += `<li class="skill" style="border: 1px solid ${categorie.color};">${skill}</li>`;
+            });
+
+            catHtml += `</ul>
                         </div>`;
+        });
+
+        var categoriesSection = document.querySelector("body>.skillsSection .categories");
+        categoriesSection.innerHTML = catHtml;
+
     });
-
-    var categoriesSection = document.querySelector("body>.skillsSection .categories");
-    categoriesSection.innerHTML = catHtml;
-
-});
 
 //fetching experiences data
 fetch("../data/experiences.json")
-.then(res => {
-    return res.json()
-})
-.then(xps => {
-    var xpsHtml = ``;
-    xps = xps.reverse();
-    xps.forEach(xp => {
-        xpsHtml += `<div class="experience">
+    .then(res => {
+        return res.json()
+    })
+    .then(xps => {
+        var xpsHtml = ``;
+        xps = xps.reverse();
+        xps.forEach(xp => {
+            xpsHtml += `<div class="experience">
                         <div class="period">
                         <span>${xp.period.from}</span>
                         <span>${xp.period.to}</span>
@@ -41,24 +41,74 @@ fetch("../data/experiences.json")
                         <h3 class="company"><a href="${xp.company.website}">${xp.company.name}</a></h3>
                         <h4 class="post">${xp.position}</h4>
                         <ul class="tasks">`;
-                        
-                        xp.tasks.forEach(task => {
-                            xpsHtml += `<li>${task}</li>`
-                        });
 
-                        xpsHtml += `</ul>
+            xp.tasks.forEach(task => {
+                xpsHtml += `<li>${task}</li>`
+            });
+
+            xpsHtml += `</ul>
                         <ul class="skills">`;
 
-                        xp.skills.forEach(skill => {
-                            xpsHtml += `<li class="skill">${skill}</li>`;
-                        });
+            xp.skills.forEach(skill => {
+                xpsHtml += `<li class="skill">${skill}</li>`;
+            });
 
-                        xpsHtml += `</ul>
+            xpsHtml += `</ul>
                         </div>
                       </div>`;
 
+        });
+
+        var xpsSection = document.querySelector("body>.experiencesSection .experiences");
+        xpsSection.innerHTML = xpsHtml;
     });
 
-    var xpsSection = document.querySelector("body>.experiencesSection .experiences");
-    xpsSection.innerHTML = xpsHtml;
-})
+
+//fetching projets data
+var prms = fetch("../data/projects.json")
+    .then(res => {
+        return res.json()
+    })
+    .then(projects => {
+        var projectsHtml = ``;
+        projects = projects.reverse();
+        projects.forEach(project => {
+            projectsHtml += `<div class="project">
+                            <div class="cover">
+                                <img src="img/projects/${project.cover}" alt="logia studios screenshot">`;
+            if (project.link != "") {
+                projectsHtml += `
+                                    <a href="${project.link}" target="blank">
+                                    <span>take a look</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="11.448" height="11.447" viewBox="0 0 11.448 11.447">
+                                    <g id="Icon_feather-external-link" data-name="Icon feather-external-link"
+                                        transform="translate(-3.5 -3.086)">
+                                        <path id="Tracé_256" data-name="Tracé 256"
+                                        d="M12.028,12.513v3.011a1,1,0,0,1-1,1H5.5a1,1,0,0,1-1-1V10a1,1,0,0,1,1-1H8.515"
+                                        transform="translate(0 -2.994)" fill="none" stroke="#090909" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-width="2" />
+                                        <path id="Tracé_257" data-name="Tracé 257" d="M22.5,4.5h3.011V7.511" transform="translate(-11.978)"
+                                        fill="none" stroke="#090909" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
+                                        <path id="Tracé_258" data-name="Tracé 258" d="M15,10.02,20.52,4.5" transform="translate(-6.987)"
+                                        fill="none" stroke="#090909" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
+                                    </g>
+                                    </svg>
+
+                                </a>
+                                    `;
+            }
+            projectsHtml += `
+                            </div>
+                            <h3>${project.name}</h3>
+                            <ul class="skills">`;
+            project.stacks.forEach(stack => {
+                projectsHtml += `<li class="skill">${stack}</li>`;
+            });
+            projectsHtml += `</ul>
+                        </div>`;
+
+        });
+
+        var projectsSection = document.querySelector("body>.projectsSection .projects");
+        projectsSection.innerHTML = projectsHtml;
+    });
--- js/main.js
+++ js/main.js
@@ -1,29 +1,99 @@
-//cursor
-var cursor = document.getElementById('customCursor');
-document.addEventListener('mousemove', function(e){
-    var cursorWidth = cursor.offsetWidth;
-    var x = e.clientX - cursorWidth/2;
-    var y = e.clientY - cursorWidth/2;
-    cursor.style.transform = 'translate('+x+'px, '+y+'px)';
-});
-var links = document.querySelectorAll('a');
-for (let i = 0; i < links.length; i++) {
-    const link = links[i];
-    link.addEventListener('mouseover', function(){
-        cursor.classList.add('on-link');
-    });
-    link.addEventListener('mouseout', function(){
-        cursor.classList.remove('on-link');
-    })
-}
 
-// var helloTween = gsap.from(".text h1", {
-//     duration: .7,
-//     rotation: 180,
-//     ease: "bounce",
-//     yoyo: true,
-//     repeat: -1
-// });
-// setTimeout(() => {
-//     helloTween.pause();
-// }, 3000);
+var overlay = document.querySelector('body>.overlay');
+
+window.addEventListener('load', function () {
+    //overlay loader animation
+    overlay.classList.add('loaded');
+    $('html').css("overflow", "auto");
+
+    /* ----------------------------
+             hero animation
+    ----------------------------*/
+
+    var helloSvg = new Vivus('hello-svg', {
+        duration: 400,
+        start: "manual"
+    });
+    var creativeSvg = new Vivus('creativeSvg', {
+        duration: 400,
+    });
+    //creating a timeline for all elements on the hero section
+    var heroTl = gsap.timeline({ paused: true });
+    heroTl.from(".hero .text h1", {
+        opacity: 0,
+        y: 100,
+        duration: .5
+    }).from(".hero .text .socials li", {
+        opacity: 0,
+        y: 50,
+        duration: .4,
+        stagger: 0.1,
+        delay: .3
+    }).from(".hero .image img", {
+        opacity: 0,
+        y: 200,
+        duration: .9,
+        ease: CustomEase.create("custom", "0.77,0,0.18,1")
+    }, "-=0.5");
+
+    $('body>.overlay>div:first-child').on('transitionend', function () {
+        //start all animations after the loder overlay has finished transitionning
+        //vivus animation
+        helloSvg.play();
+        setTimeout(() => {
+            heroTl.play();
+        }, 2000);
+    });
+
+
+});
+
+
+//project animation
+var elasticEase = CustomEase.create("custom", "0.73,-0.67,0,1.67");
+
+prms.then(() => {
+    $('.projectsSection .project').each(function (i, project) {
+        var takeAlook = $(this).find('a');
+        var tlTakeALook = gsap.timeline({ paused: true });
+        tlTakeALook.to(takeAlook, {
+            y: -45,
+            duration: .4,
+            ease: elasticEase,
+        }).to(takeAlook, {
+            paddingLeft: 100,
+            duration: .5,
+            ease: elasticEase
+        });
+
+
+
+        $(project).hover(() => tlTakeALook.play(), () => tlTakeALook.reverse());
+        function startAnim() {
+            tlTakeALook.play();
+        }
+    });
+
+    /* ----------------------------
+             custom cursor
+    ----------------------------*/
+    var cursor = document.getElementById('customCursor');
+    document.addEventListener('mousemove', function (e) {
+        var cursorWidth = cursor.offsetWidth;
+        var x = e.clientX - cursorWidth / 2;
+        var y = e.clientY - cursorWidth / 2;
+        cursor.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+    });
+    var links = document.querySelectorAll('a');
+    for (let i = 0; i < links.length; i++) {
+        const link = links[i];
+        link.addEventListener('mouseover', function () {
+            cursor.classList.add('on-link');
+        });
+        link.addEventListener('mouseout', function () {
+            cursor.classList.remove('on-link');
+        })
+    }
+
+
+});
